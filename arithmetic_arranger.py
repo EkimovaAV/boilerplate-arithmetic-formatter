378b0ac3b02eaf38b989f7ed070d70da831da93d
Настя <97758296+EkimovaAV@users.noreply.github.com>
2022-11-23 00:10:58 +0300

    1st commit
    without mistake in 14

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,17 @@
-def arithmetic_arranger(problems):
+import re
+def arithmetic_arranger(problems, getanswer=False):
+    # too many problems
 
+    if len(problems) > 5:
+        return "Error: Too many problems."
 
+    firstNum = ""
+    secondNum = ""
+    lines = ""
+    result = ""
+    for item in problems:
+            if re.search("[^\s0-9.+-]", item):
+                if re.search(["/*"], item) :
+                    return "Error: Operator must be '+' or '-'."
+                return "Error: Numbers must only contain digits."
     return arranged_problems
