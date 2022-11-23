6d64fe0a56f08abccb29ad1033ec97a9b5635131
Настя <97758296+EkimovaAV@users.noreply.github.com>
2022-11-23 22:35:30 +0300

    it still not passes

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -25,19 +25,20 @@ def arithmetic_arranger(problems, getanswer=False):
             if len(leftOper) > 4 or len(rightOper) > 4:
                 return "Error: Numbers cannot be more than four digits."
             else:
-                if len(leftOper) >=len(rightOper):
-                    length=len(leftOper)-len(rightOper)
+                #right i think
+                if len(leftOper) >= len(rightOper):
+                    length = len(leftOper) - len(rightOper)
                     lines=len(leftOper)
                     #разница между 1 и 2 оператором?
                     oneline += f"  {leftOper}    "
                     secondline +=  f"{operator}"+ (length)*' '+' ' +(rightOper)+"    "
-                    poloski+=lines*'-'+'--    '
+                    poloski+=lines*'-' + '--    '
                 else:
-                    length=len(rightOper)-len(leftOper)
-                    lines=len(rightOper)
-                    oneline+=(length)*' '+ f"  {leftOper}    "
-                    secondline+=f"{operator} {rightOper}    "
-                    poloski+=lines*'-'+'--    '
+                    length = len(rightOper)-len(leftOper)
+                    lines = len(rightOper)
+                    oneline += (length)*' '+ f"  {leftOper}    "
+                    secondline += f"{operator} {rightOper}    "
+                    poloski += lines*'-'+'--    '
 
-    arranged_problems+= oneline+"\n"+secondline+"\n"+poloski
+    arranged_problems += oneline + "\n" + secondline + "\n" + poloski
     return arranged_problems
